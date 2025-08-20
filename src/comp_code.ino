0b87d7ccf2b5fa167a00d8b7991a36240664d670
S Strachan <sm_strachan@outlook.com>
2025-08-20 12:38:55 +1200

    Shortened delay before backing off to 125.
    Slipped more on second run but first was smooth

--- src/comp_code.ino
+++ src/comp_code.ino
@@ -140,7 +140,7 @@ void loop() {
 
     // Robot starts to tip ramp
 
-    delay(150);
+    delay(125);
     Lstepper.setMaxSpeed(1500);
     Rstepper.setMaxSpeed(1500);
     Lstepper.moveRelative(-170);
