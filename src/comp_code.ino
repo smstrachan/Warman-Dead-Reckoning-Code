0452a8315823ccd0ae3415c4f66cf72b3153ca9d
S Strachan <sm_strachan@outlook.com>
2025-08-20 18:20:37 +1200

    Increased initial distance up ramp.
    Smooth and complete

--- src/comp_code.ino
+++ src/comp_code.ino
@@ -134,8 +134,8 @@ void loop() {
     // Lstepper.setMaxSpeed(200);
     // Rstepper.setMaxSpeed(200);
 
-    Lstepper.moveRelative(1200);
-    Rstepper.moveRelative(1200);
+    Lstepper.moveRelative(1210);
+    Rstepper.moveRelative(1210);
     while (Lstepper.distanceToGo() != 0 || Rstepper.distanceToGo() != 0){
       Lstepper.run();
       Rstepper.run();
