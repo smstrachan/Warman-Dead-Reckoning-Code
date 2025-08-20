9065ed9c9615dbc082145afe7cea1a9d903c4140
S Strachan <sm_strachan@outlook.com>
2025-08-20 12:06:18 +1200

    Changed turns to 350

--- src/comp_code.ino
+++ src/comp_code.ino
@@ -120,8 +120,8 @@ void loop() {
     }
     // Robot has balls
 
-    Lstepper.moveRelative(-355);
-    Rstepper.moveRelative(355);
+    Lstepper.moveRelative(-350);
+    Rstepper.moveRelative(350);
     while (Lstepper.distanceToGo() != 0 || Rstepper.distanceToGo() != 0){
       Lstepper.run();
       Rstepper.run();
@@ -165,8 +165,8 @@ void loop() {
   // Robot is at the end of the ramp
   // TODO: tune numbers for end 
 
-    Lstepper.moveRelative(-335);
-    Rstepper.moveRelative(335);
+    Lstepper.moveRelative(-350);
+    Rstepper.moveRelative(350);
     while (Lstepper.distanceToGo() != 0 || Rstepper.distanceToGo() != 0){
       Lstepper.run();
       Rstepper.run();
